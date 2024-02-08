60983cdf86967263376f6ecbcb284e739c9ae557
Thaisncp <tncp2003@gmail.com>
2024-02-08 00:18:25 -0500

    Mensaje del api

--- src/fragment/Api.jsx
+++ src/fragment/Api.jsx
@@ -1,38 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import Navbar from './Navbar';
-import Footer from './Footer';
 import AceEditor from 'react-ace';
-import 'ace-builds/src-noconflict/theme-twilight';
+import 'ace-builds/src-noconflict/theme-cobalt'; // Importar el tema cobalt desde ace-builds
 import 'ace-builds/src-noconflict/mode-json';
-import 'ace-builds/src-noconflict/ext-language_tools';
+import 'ace-builds/src-noconflict/ext-language_tools'
+import Navbar from './Navbar';
 
-const ApiRequest = ({ method, endpoint, outputKey, outputValue }) => (
-    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <div >
-            <div className="d-flex justify-content-between theme-ambiance">
-                <p className="accordion-header metodo">{method}</p>
-            </div>
-            <div className="alert alert-info theme-ambiance" role="alert">
-                {endpoint}
-            </div>
-        </div>
-        <div key={outputKey}>
-        {outputValue && (
-            <AceEditor
-                mode="json"
-                theme="twilight"
-                name={`output-editor-${outputKey}`}
-                editorProps={{ $blockScrolling: Infinity }}
-                value={outputValue || ''}
-                style={{ height: '80%', fontSize: '20px', width: '100%' }}
-                readOnly={true}
-            />
-        )}
-        </div>
-    </div>
-);
-
-const Api = () => {
+function Api() {
     const URL_BACKEND_API = "https://computacion.unl.edu.ec/uv/api/";
     const lista_peticiones_get = {
         peticion1: { metodo: 'LISTA DISPOSITIVOS', peticion: URL_BACKEND_API + 'listar' },
@@ -45,6 +18,8 @@ const Api = () => {
         peticion2: { metodo: 'PROMEDIO POR SEMANAS', peticion: URL_BACKEND_API + 'medicionSemana' },
         peticion3: { metodo: 'PROMEDIO POR DIA', peticion: URL_BACKEND_API + 'medicionDia' },
     };
+
+    // Estado para almacenar la salida de cada petición
     const [outputs, setOutputs] = useState({});
 
     useEffect(() => {
@@ -55,21 +30,48 @@ const Api = () => {
                     throw new Error(`HTTP error! Status: ${response.status}`);
                 }
                 const data = await response.json();
-                setOutputs((prevState) => ({
+                setOutputs(prevState => ({
                     ...prevState,
-                    [key]: JSON.stringify(data, null, 2),
+                    [key]: JSON.stringify(data, null, 2)
                 }));
             } catch (error) {
                 console.error('Error fetching API:', error);
-                setOutputs((prevState) => ({
+                setOutputs(prevState => ({
                     ...prevState,
-                    [key]: `Error fetching API: ${error.message}`,
+                    [key]: `Error fetching API: ${error.message}`
                 }));
             }
         };
-        Object.keys(lista_peticiones_get).forEach((key) => fetchData(`get_${key}`, lista_peticiones_get[key].peticion));
-        Object.keys(lista_peticiones_post).forEach((key) => fetchData(`post_${key}`, lista_peticiones_post[key].peticion));
-    }, [lista_peticiones_get, lista_peticiones_post]);
+        Object.entries(lista_peticiones_get).forEach(([key, item]) => fetchData(key, item.peticion));
+        Object.entries(lista_peticiones_post).forEach(([key, item]) => fetchData(key, item.peticion));
+    }, []); // El array vacío como segundo argumento asegura que el efecto se ejecute solo una vez al cargar la página
+
+    const renderPeticiones = (lista) => {
+        return Object.keys(lista).map((key) => (
+            <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '20px' }} className="col-xs-12 col-sm-12 col-md-6 col-lg-6" key={key}>
+                <div style={{ marginBottom: '10px', flex: 1 }}>
+                <div className="d-flex justify-content-between">
+                <h6 style={{ color: 'black', textAlign: 'left' }}>{lista[key].metodo}</h6>
+            </div>
+            <div className="alert alert-info" role="alert">
+            <h4 style={{ textAlign: 'left' }}>{lista[key].peticion}</h4>
+            </div>
+                   
+                  
+                </div>
+                <div> {/* Agregamos margen izquierdo y derecho de 10px */}
+                <AceEditor
+                        mode="json"
+                        theme="cobalt"
+                        name={`output-editor-${key}`}
+                        editorProps={{ $blockScrolling: Infinity }}
+                        value={outputs[key] || ''} // Utilizar la salida correspondiente a la petición actual
+                        style={{ height: '200px', fontSize: "20px", width: 'calc(100% - 20px)' }}
+                        readOnly={true} />
+                </div>
+            </div>
+        ));
+    };
 
     return (
         <>
@@ -77,41 +79,25 @@ const Api = () => {
             <main className="p-5">
                 <div className="card p-4" style={{ overflow: 'auto', maxHeight: '780px' }}>
                     <div className="p-4 row">
-                        <div className="col-sm-10">
+                        <div className="col-sm-10" style={{alignContent: 'center'}}>
                             <h2>Listado de peticiones disponibles</h2>
                         </div>
                     </div>
                     <div>
-                        <h1>GET</h1>
-                        <div className="row">
-                            {Object.keys(lista_peticiones_get).map((clave, index) => (
-                                <ApiRequest
-                                    key={index}
-                                    method={lista_peticiones_get[clave].metodo}
-                                    endpoint={lista_peticiones_get[clave].peticion}
-                                    outputKey={`get_${clave}`}
-                                    outputValue={outputs[`get_${clave}`]}
-                                />
-                            ))}
+                        <h3>GET</h3>
+                        <div className='row'>
+                            {renderPeticiones(lista_peticiones_get)}
                         </div>
-                        <h1>POST</h1>
-                        <div className="row">
-                            {Object.keys(lista_peticiones_post).map((clave, index) => (
-                                <ApiRequest
-                                    key={index}
-                                    method={lista_peticiones_post[clave].metodo}
-                                    endpoint={lista_peticiones_post[clave].peticion}
-                                    outputKey={`post_${clave}`}
-                                    outputValue={outputs[`post_${clave}`]}
-                                />
-                            ))}
+
+                        <h3>POST</h3>
+                        <div className='row'>
+                            {renderPeticiones(lista_peticiones_post)}
                         </div>
                     </div>
                 </div>
             </main>
-            <Footer />
         </>
     );
-};
+}
 
 export default Api;

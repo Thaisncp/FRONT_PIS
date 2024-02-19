66fe8af7c9cc016f28bb340b72b4de8721592704
Thaisncp <tncp2003@gmail.com>
2024-02-19 15:54:28 -0500

    finalv3.0

--- src/fragment/Navbar.jsx
+++ src/fragment/Navbar.jsx
@@ -6,6 +6,7 @@ import { Modal, Button } from 'react-bootstrap';
 import Comentario from './Comentario';
 import '../components/css/Comentario.css';
 import '../components/css/Navbar.css';
+import { BiHouse } from 'react-icons/bi';  
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -41,49 +42,60 @@ const Navbar = () => {
             {rol === 'ADMINISTRADOR' && (
               <>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">Inicio</a>
+                  <a className="nav-link active" aria-current="page" href="/">
+                    <img src= "https://cdn.icon-icons.com/icons2/62/PNG/96/blue_home_house_12608.png"alt="icono" className="icon-in-o"></img>  HOME  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/validacion-usuarios">Validacion usuarios</a>
+                  <a className="nav-link active" aria-current="page" href="/validacion-usuarios">
+                  <img src= "https://cdn.icon-icons.com/icons2/62/PNG/96/blue_users_customers_clients_people_12438.png"alt="icono" className="icon-in-o"></img>  Validacion usuarios  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/api">Api</a>
+                  <a className="nav-link active" aria-current="page" href="/api">
+                  <img src= "https://cdn.icon-icons.com/icons2/62/PNG/96/blue_copy_paste_12539.png"alt="icono" className="icon-in-o"></img>  Api  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/usuarios-aceptados">Usuarios Aceptados</a>
+                  <a className="nav-link active" aria-current="page" href="/usuarios-aceptados">
+                  <img src= "https://cdn.icon-icons.com/icons2/62/PNG/96/blue_accept_ok_apply_12418.png"alt="icono" className="icon-in-o"></img>  Usuarios Aceptados  </a>
                 </li>
               </>
             )}
             {rol === 'USUARIO' && (
               <>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">Inicio</a>
+                  <a className="nav-link active" aria-current="page" href="/">
+                    <img src= "https://cdn.icon-icons.com/icons2/62/PNG/96/blue_home_house_12608.png"alt="icono" className="icon-in-o"></img>  HOME  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/api">Api</a>
+                  <a className="nav-link active" aria-current="page" href="/api">
+                  <img src= "https://cdn.icon-icons.com/icons2/62/PNG/96/blue_copy_paste_12539.png"alt="icono" className="icon-in-o"></img>  Api  </a>
                 </li>
                 <li className="nav-item">
-                <a className="nav-link active" aria-current="page" onClick={handleAbrirComentariosModal}>Añadir Comentario</a>
+                <a className="nav-link active" aria-current="page" onClick={handleAbrirComentariosModal}>
+                <img src= "https://cdn.icon-icons.com/icons2/62/PNG/96/bluequestion_azu_12470.png"alt="icono" className="icon-in-o"></img>  Añadir Comentario  </a>
                 </li>
               </>
             )}
             {!sesionIniciada && (
               <>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">Inicio</a>
+                  <a className="nav-link active" aria-current="page" href="/">
+                    <img src= "https://cdn.icon-icons.com/icons2/62/PNG/96/blue_home_house_12608.png"alt="icono" className="icon-in-o"></img>  HOME  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/inicio-sesion">Login</a>
+                  <a className="nav-link active" aria-current="page" href="/inicio-sesion">
+                  <img src= "https://cdn.icon-icons.com/icons2/62/PNG/96/blue_user_client_person_12581.png"alt="icono" className="icon-in-o"></img>  Inicio Sesion  </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/registro">Registro</a>
+                  <a className="nav-link active" aria-current="page" href="/registro">
+                  <img src= "https://cdn.icon-icons.com/icons2/62/PNG/96/bluepen_azul__12511.png"alt="icono" className="icon-in-o"></img>  Registro  </a>
                 </li>
               </>
             )}
           </ul>
           {sesionIniciada && (
             <span className="navbar-text">
-              <a className="nav-link" href="/inicio-sesion" onClick={handleCerrarSesion}>Cerrar sesión</a>
+              <a className="nav-link" href="/inicio-sesion" onClick={handleCerrarSesion}>
+              <img src= "https://cdn.icon-icons.com/icons2/62/PNG/96/bluearrowright_azul_flech_12476.png"alt="icono" className="icon-in-o"></img>  Cerrar sesión</a>
             </span>
           )}
         </div>
